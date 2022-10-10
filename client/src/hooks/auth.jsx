8a9a588d2dc5a2ce0ac3f30825a3371c2853143c
Yotam Berkowitz <yotamberk@gmail.com>
2022-10-10 08:46:15 +0300

    add localStorage saved token

--- client/src/hooks/auth.jsx
+++ client/src/hooks/auth.jsx
@@ -7,22 +7,32 @@ const fakeAuth = () => new Promise((resolve) => {
   setTimeout(() => resolve('2342f2f1d131rf12'), 250)
 })
 
+const getToken = () => {
+  const tokenString = localStorage.getItem('token')
+  // const userToken = JSON.parse(tokenString)
+  // return userToken?.token
+  return tokenString
+}
+
 function AuthProvider({ children }) {
   const navigate = useNavigate()
   const location = useLocation()
 
-  const [token, setToken] = React.useState(null)
+  const [token, setToken] = React.useState(getToken())
 
   const handleLogin = async () => {
     const recievedToken = await fakeAuth()
 
     setToken(recievedToken)
+    localStorage.setItem('token', JSON.stringify(recievedToken))
+
     const origin = location.state?.from?.pathname || 'customers/overview'
     navigate(origin)
   }
 
   const handleLogout = () => {
     setToken(null)
+    localStorage.removeItem('token')
   }
 
   const value = React.useMemo(() => ({
