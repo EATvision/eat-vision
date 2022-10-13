05e2a4e9af9a0626a6e9568d48b356f5bb1eeb51
Yotam Berkowitz <yotamberk@gmail.com>
2022-10-13 20:44:35 +0300

    add theming

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom'
 
 import i18next from 'i18next'
+import { ThemeProvider, useTheme } from '@mui/material'
 import DishesPage from './components/DinersView/DishesPage'
 
 import DinerView from './views/DinerView'
@@ -25,6 +26,7 @@ import DishPage from './components/CustomersView/DishesManagerPage/DishPage'
 import CustomerLoginPage from './components/CustomersView/CustomerLoginPage'
 import AuthProvider from './hooks/auth'
 import ProtectedRoute from './components/ProtectedRoute'
+import theme from './theme'
 
 function App() {
   const [filters, setFilters] = React.useState(defaultFilters)
@@ -33,43 +35,45 @@ function App() {
   document.body.dir = i18next.dir()
 
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/diners/kitchens" />} />
-        <Route path="/diners" element={<DinerView />}>
-          <Route index path="kitchens" element={<KitchensPage />} />
-          <Route path="kitchens/:kitchenId" element={<KitchenPage />}>
-            <Route path="menus" element={<MenusPage />} />
-            <Route path="menus/:menuId" element={<MenuPage filters={filters} setDishes={setDishes} />}>
-              <Route index element={<GreetingPage setFilters={setFilters} />} />
-              <Route path="filters/:step" element={<FiltersWizardPage dishes={dishes} filters={filters} setFilters={setFilters} />} />
-              <Route path="dishes" element={<DishesPage filters={filters} dishes={dishes} setDishes={setDishes} />} />
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/diners/kitchens" />} />
+          <Route path="/diners" element={<DinerView />}>
+            <Route index path="kitchens" element={<KitchensPage />} />
+            <Route path="kitchens/:kitchenId" element={<KitchenPage />}>
+              <Route path="menus" element={<MenusPage />} />
+              <Route path="menus/:menuId" element={<MenuPage filters={filters} setDishes={setDishes} />}>
+                <Route index element={<GreetingPage setFilters={setFilters} />} />
+                <Route path="filters/:step" element={<FiltersWizardPage dishes={dishes} filters={filters} setFilters={setFilters} />} />
+                <Route path="dishes" element={<DishesPage filters={filters} dishes={dishes} setDishes={setDishes} />} />
+              </Route>
             </Route>
+
+            <Route path="filters" element={<Navigate replace to="/diners/filters/1" />} />
+            <Route path="filters/:step" element={<FiltersWizardPage dishes={dishes} filters={filters} setFilters={setFilters} />} />
           </Route>
 
-          <Route path="filters" element={<Navigate replace to="/diners/filters/1" />} />
-          <Route path="filters/:step" element={<FiltersWizardPage dishes={dishes} filters={filters} setFilters={setFilters} />} />
-        </Route>
+          <Route path="customers" element={<CustomerView />}>
+            <Route index element={<Navigate replace to="/customers/login" />} />
+            <Route path="login" element={<CustomerLoginPage />} />
+            <Route path="overview" element={<ProtectedRoute><OverviewPage /></ProtectedRoute>} />
+            <Route path="dishes" element={<ProtectedRoute><DishesManagerPage /></ProtectedRoute>} />
+            <Route path="dishes/:dishId" element={<ProtectedRoute><DishPage /></ProtectedRoute>} />
+            <Route path="menus" element={<ProtectedRoute><MenusManagerPage /></ProtectedRoute>} />
+          </Route>
 
-        <Route path="customers" element={<CustomerView />}>
-          <Route index element={<Navigate replace to="/customers/login" />} />
-          <Route path="login" element={<CustomerLoginPage />} />
-          <Route path="overview" element={<ProtectedRoute><OverviewPage /></ProtectedRoute>} />
-          <Route path="dishes" element={<ProtectedRoute><DishesManagerPage /></ProtectedRoute>} />
-          <Route path="dishes/:dishId" element={<ProtectedRoute><DishPage /></ProtectedRoute>} />
-          <Route path="menus" element={<ProtectedRoute><MenusManagerPage /></ProtectedRoute>} />
-        </Route>
-
-        <Route
-          path="*"
-          element={(
-            <main style={{ padding: '1rem' }}>
-              <p>There&apos;s nothing here!</p>
-            </main>
+          <Route
+            path="*"
+            element={(
+              <main style={{ padding: '1rem' }}>
+                <p>There&apos;s nothing here!</p>
+              </main>
           )}
-        />
-      </Routes>
-    </AuthProvider>
+          />
+        </Routes>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 
---
+++ client/src/theme.jsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles'
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#009688',
+    },
+    secondary: {
+      main: '#ff5722',
+    },
+  },
+})
