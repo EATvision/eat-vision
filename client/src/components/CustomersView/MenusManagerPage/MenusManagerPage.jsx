d28944585448cac1983fe2f2ecae8b111dead20a
Samuel Arbibe <samuel.arbibe@gmail.com>
2022-11-17 13:39:37 +0200

    Resolve eslint errors

--- client/src/components/CustomersView/MenusManagerPage/MenusManagerPage.jsx
+++ client/src/components/CustomersView/MenusManagerPage/MenusManagerPage.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
 
 function MenusManagerPage() {
-  const { i18n } = useTranslation()
-
-  return (
-    <div>
-      MENUS LIST
-    </div>
-  )
+  return <div>MENUS LIST</div>
 }
 
 export default MenusManagerPage
