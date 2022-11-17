bf79ce483394601c35fd51f15f6db7956206fac5
EdenIdith <idith@eat.vision>
2022-11-17 17:49:10 +0200

    Add Sentry support

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
 import 'tw-elements'
 import './index.css'
-
 import './i18n'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+Sentry.init({
+  dsn: 'https://05b360e64612477db7f69c6da52f7ff1@o4504174016069632.ingest.sentry.io/4504174018953216',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
