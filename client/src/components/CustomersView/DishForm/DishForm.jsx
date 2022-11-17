e218b9bc1d59416da6e1d1d17ef1f61a60e176b2
Samuel Arbibe <samuel.arbibe@gmail.com>
2022-11-17 13:39:37 +0200

    Resolve eslint errors

--- client/src/components/CustomersView/DishForm/DishForm.jsx
+++ client/src/components/CustomersView/DishForm/DishForm.jsx
@@ -1,12 +1,7 @@
 import axios from 'axios'
 import React from 'react'
-import {
-  Button, Box, TextField, useTheme, InputAdornment,
-} from '@mui/material'
-import {
-  Formik,
-  Form,
-} from 'formik'
+import { Button, Box, TextField, useTheme, InputAdornment } from '@mui/material'
+import { Formik, Form } from 'formik'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import DishFormSelect from './DishFormSelect'
@@ -42,7 +37,7 @@ function DishForm({ data }) {
     <Box sx={{ padding: theme.spacing(2), textAlign: 'start' }}>
       <Formik
         initialValues={data || initialDish}
-        validate={(values) => {
+        validate={() => {
           const errors = {}
 
           return errors
@@ -52,6 +47,7 @@ function DishForm({ data }) {
             if (values?._id) {
               await axios.put(`/api/dishes/${values._id}`, values)
             } else {
+              // eslint-disable-next-line no-unused-vars
               const { _id, ...dishWithNoId } = values
               await axios.post('/api/dishes', dishWithNoId)
             }
@@ -63,73 +59,94 @@ function DishForm({ data }) {
           setSubmitting(false)
         }}
       >
-        {
-          ({
-            values, touched, errors, handleChange,
-          }) => (
-            <Form>
-              <TextField
-                required
-                margin="normal"
-                fullWidth
-                id="name"
-                name="name"
-                label="name"
-                value={values.name}
-                onChange={handleChange}
-                error={touched.name && Boolean(errors.name)}
-                helperText={touched.name && errors.name}
-              />
+        {({ values, touched, errors, handleChange }) => (
+          <Form>
+            <TextField
+              required
+              margin="normal"
+              fullWidth
+              id="name"
+              name="name"
+              label="name"
+              value={values.name}
+              onChange={handleChange}
+              error={touched.name && Boolean(errors.name)}
+              helperText={touched.name && errors.name}
+            />
 
-              <TextField
-                margin="normal"
-                fullWidth
-                id="description"
-                name="description"
-                label="description"
-                type="description"
-                value={values.description}
-                onChange={handleChange}
-                error={touched.description && Boolean(errors.description)}
-                helperText={touched.description && errors.description}
-              />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="description"
+              name="description"
+              label="description"
+              type="description"
+              value={values.description}
+              onChange={handleChange}
+              error={touched.description && Boolean(errors.description)}
+              helperText={touched.description && errors.description}
+            />
 
-              <TextField
-                margin="normal"
-                fullWidth
-                id="price"
-                name="price"
-                label="price"
-                type="number"
-                InputProps={{
-                  inputMode: 'numeric',
-                  pattern: '[0-9]*',
-                  endAdornment: <InputAdornment position="start">{kitchen?.currency}</InputAdornment>,
-                }}
-                value={values.price}
-                onChange={handleChange}
-                error={touched.price && Boolean(errors.price)}
-                helperText={touched.price && errors.price}
-              />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="price"
+              name="price"
+              label="price"
+              type="number"
+              InputProps={{
+                inputMode: 'numeric',
+                pattern: '[0-9]*',
+                endAdornment: (
+                  <InputAdornment position="start">
+                    {kitchen?.currency}
+                  </InputAdornment>
+                ),
+              }}
+              value={values.price}
+              onChange={handleChange}
+              error={touched.price && Boolean(errors.price)}
+              helperText={touched.price && errors.price}
+            />
 
-              <IngredientFormSelect label="mandatory ingredients" name="recipe.mandatory" disableAdditionalPrices />
+            <IngredientFormSelect
+              label="mandatory ingredients"
+              name="recipe.mandatory"
+              disableAdditionalPrices
+            />
 
-              <IngredientFormSelect label="choice ingredients" name="recipe.choice" />
+            <IngredientFormSelect
+              label="choice ingredients"
+              name="recipe.choice"
+            />
 
-              <IngredientFormSelect label="excludable ingredients" name="recipe.excludable" disableAdditionalPrices />
+            <IngredientFormSelect
+              label="excludable ingredients"
+              name="recipe.excludable"
+              disableAdditionalPrices
+            />
 
-              <DishFormSelect label="sideDish ingredients" name="recipe.sideDish" disableAdditionalPrices />
+            <DishFormSelect
+              label="sideDish ingredients"
+              name="recipe.sideDish"
+              disableAdditionalPrices
+            />
 
-              <IngredientFormSelect label="addable ingredients" name="recipe.addableIngredients" />
+            <IngredientFormSelect
+              label="addable ingredients"
+              name="recipe.addableIngredients"
+            />
 
-              <DishFormSelect label="addable dishes" name="recipe.addableDishes" />
+            <DishFormSelect
+              label="addable dishes"
+              name="recipe.addableDishes"
+            />
 
-              <Button color="primary" variant="contained" fullWidth type="submit">
-                {data?._id ? 'Save' : 'Submit'}
-              </Button>
-            </Form>
-          )
-        }
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              {data?._id ? 'Save' : 'Submit'}
+            </Button>
+          </Form>
+        )}
       </Formik>
     </Box>
   )
--- client/src/components/CustomersView/OverviewPage.jsx
+++ client/src/components/CustomersView/OverviewPage.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 function OverviewPage() {
+  // eslint-disable-next-line no-unused-vars
   const { i18n } = useTranslation()
 
-  return (
-    <div>
-      Overview
-    </div>
-  )
+  return <div>Overview</div>
 }
 
 export default OverviewPage
