57b074c7c7f354c97b3289961102f863b480984f
Samuel Arbibe <samuel.arbibe@gmail.com>
2022-11-18 12:10:14 +0200

    Refactor Kitchen page

--- client/src/components/KitchenSelector.jsx
+++ client/src/components/KitchenSelector.jsx
@@ -1,44 +1,65 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import {
-  FormControl, InputLabel, MenuItem, Select, Typography,
+  Select,
+  MenuItem,
+  InputLabel,
+  Typography,
+  FormControl,
 } from '@mui/material'
-import { useKitchens } from '../hooks/kitchens'
-import { useKitchen } from '../contexts/kitchen'
+import { Add } from '@mui/icons-material'
+import { useKitchens } from 'hooks/kitchens'
+import { useKitchen } from 'contexts/kitchen'
+import { addKitchen } from 'api/kitchens'
 
 function KitchenSelector() {
-  const { kitchens, isLoading } = useKitchens()
+  const { t } = useTranslation()
   const { kitchenId, onSetKitchenId } = useKitchen()
+  const { kitchens, isLoading, mutate: mutateKitchens } = useKitchens()
 
-  React.useEffect(() => {
-    if (!kitchenId && kitchens?.[0]?.id) {
-      onSetKitchenId(kitchens[0].id)
+  useEffect(() => {
+    const firstKitchenId = kitchens?.[0].id
+    if (!kitchenId && firstKitchenId) {
+      onSetKitchenId(firstKitchenId)
     }
-  }, [kitchens])
+  }, [kitchenId, kitchens, onSetKitchenId])
 
-  const handleChange = (event) => {
-    onSetKitchenId(event.target.value)
+  const handleChange = async (event) => {
+    const value = event.target.value
+    if (value === 'new') {
+      try {
+        const { insertedId: insertedKitchenId } = await addKitchen({
+          name: 'New Kitchen',
+        })
+        await mutateKitchens()
+        onSetKitchenId(insertedKitchenId)
+      } catch (error) {
+        console.log('Could not add kitchen')
+      }
+    } else {
+      onSetKitchenId(value)
+    }
   }
 
   if (!kitchenId || isLoading) return null
 
   if (kitchens?.length === 0) {
-    return (
-      <Typography>{kitchens[0].name}</Typography>
-    )
+    return <Typography>{kitchens[0].name}</Typography>
   }
 
   return (
     <FormControl sx={{ minWidth: 120 }} size="small">
       <InputLabel id="demo-select-small">kitchens</InputLabel>
-      <Select
-        value={kitchenId}
-        onChange={handleChange}
-      >
-        {
-          kitchens?.map((kitchen) => (
-            <MenuItem key={kitchen.id} value={kitchen.id}>{kitchen.name}</MenuItem>
-          ))
-        }
+      <Select value={kitchenId} onChange={handleChange}>
+        {kitchens?.map((kitchen) => (
+          <MenuItem key={kitchen.id} value={kitchen.id}>
+            {kitchen.name}
+          </MenuItem>
+        ))}
+        <MenuItem key={'new'} value={'new'}>
+          <Add />
+          {t('New Kitchen')}
+        </MenuItem>
       </Select>
     </FormControl>
   )
---
+++ client/src/schemas/kitchen.js
@@ -0,0 +1,22 @@
+import yup from 'schemas/yup'
+
+const kitchenValidationSchema = yup.object({
+  name: yup.string().required('Name is required'),
+  website: yup
+    .string()
+    .url('Website url must be valid')
+    .required('Website is required'),
+  location: yup
+    .array()
+    .of(yup.string())
+    .required('Location is required')
+    .min(1, 'You must provide at least one location'),
+  intentions: yup
+    .array()
+    .of(yup.string())
+    .required('Intensions are required')
+    .min(1, 'You must provide at least one intension'),
+  currency: yup.string().required('Currency is required').nullable(),
+})
+
+export default kitchenValidationSchema
