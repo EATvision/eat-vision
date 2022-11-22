a02b9474956fa8029fe3c50a8308130491374b92
EdenIdith <idith@eat.vision>
2022-11-22 14:06:25 +0200

    Add support in environment param

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const Tracing = require("@sentry/tracing")
 
 
 Sentry.init({
-  dsn: "https://a89344f4de9b4f49b13683a9544538c6@o4504174016069632.ingest.sentry.io/4504189512843264",
-
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.ENVIRONMENT,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
