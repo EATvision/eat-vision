aa81460f78839347afbefdcbe9ff2082f56c44c6
Yotam Berkowitz <yotamberk@gmail.com>
2022-11-23 10:20:02 +0200

    fix positioning in diner view

--- client/src/views/DinerView.jsx
+++ client/src/views/DinerView.jsx
@@ -9,9 +9,6 @@ import RTL from 'components/RTL'
 import useIsRTL from 'hooks/useRTL'
 import { ProvideDinerUser } from 'contexts/diner'
 
-const NAV_BAR_HEIGHT = 48
-const FOOTER_HEIGHT = 50
-
 function DinerView({ dishes }) {
   const isRTL = useIsRTL()
 
@@ -22,18 +19,18 @@ function DinerView({ dishes }) {
           className="App"
           sx={{
             position: 'relative',
-            paddingTop: `${NAV_BAR_HEIGHT}px`,
             display: 'flex',
             flexDirection: 'column',
+            height: '100vh',
           }}
         >
           <NavBar />
           <Box
             sx={{
-              height: `calc(100vh - ${NAV_BAR_HEIGHT}px - ${FOOTER_HEIGHT}px)`,
               position: 'relative',
               display: 'flex',
               justifyContent: 'center',
+              flex: 1,
             }}
           >
             <Outlet />
