d7b2de90d1584f7b4dbc6492d47ac1ce969a8fe1
Yotam Berkowitz <yotamberk@gmail.com>
2022-11-30 22:11:57 +0200

    add ingredient tree info

--- client/src/components/DinersView/DishCard/IngredientsInfo.jsx
+++ client/src/components/DinersView/DishCard/IngredientsInfo.jsx
@@ -1,35 +1,83 @@
 import React from 'react'
-import { List, ListItem } from '@mui/material'
-import { useGetComponentLabel, useV1IngredientsByIds } from 'hooks/ingredients'
+import { Typography } from '@mui/material'
+import { useV1IngredientsByIds } from 'hooks/ingredients'
 import IngredientsTree from 'components/DinersView/DishCard/IngredientsTree'
-
-const _keyBy = require('lodash/keyBy')
+import { t } from 'i18next'
 
 function IngredientsInfo({ data }) {
-  const relevantIds = [
-    ...data.recipe.mandatory.map((c) => c.id),
-    ...data.recipe.excludable.map((c) => c.id),
-    ...data.recipe.putaside.map((c) => c.id),
-    ...data.recipe.choice.map((c) => c.id),
-    ...data.recipe.sideDish.map((c) => c.id),
-    ...data.recipe.addableIngredients.map((c) => c.id),
-    ...data.recipe.addableDishes.map((c) => c.id),
-  ]
+  const { ingredients: mandatoryIngredients } = useV1IngredientsByIds(
+    data.recipe.mandatory.map((c) => c.id)
+  )
 
-  const { ingredients } = useV1IngredientsByIds(relevantIds)
-  const ingredientsById = _keyBy(ingredients, 'id')
-  const getComponentLabel = useGetComponentLabel()
+  const { ingredients: excludableIngredients } = useV1IngredientsByIds(
+    data.recipe.excludable.map((c) => c.id)
+  )
+
+  const { ingredients: putasideIngredients } = useV1IngredientsByIds(
+    data.recipe.putaside.map((c) => c.id)
+  )
+
+  const { ingredients: choiceIngredients } = useV1IngredientsByIds(
+    data.recipe.choice.map((c) => c.id)
+  )
+
+  const { ingredients: sideDishIngredients } = useV1IngredientsByIds(
+    data.recipe.sideDish.map((c) => c.id)
+  )
+
+  const { ingredients: addableIngredientsIngredients } = useV1IngredientsByIds(
+    data.recipe.addableIngredients.map((c) => c.id)
+  )
+
+  const { ingredients: addableDishesIngredients } = useV1IngredientsByIds(
+    data.recipe.addableDishes.map((c) => c.id)
+  )
 
   return (
     <>
-      <IngredientsTree />
-      <List>
-        {ingredients?.map((ing) => (
-          <ListItem key={ing.id}>
-            {getComponentLabel(ingredientsById?.[ing.id])}
-          </ListItem>
-        ))}
-      </List>
+      {mandatoryIngredients?.length > 0 && (
+        <>
+          <Typography>{t('mandatory_ingredients')}</Typography>
+          <IngredientsTree ingredients={mandatoryIngredients} />
+        </>
+      )}
+
+      {excludableIngredients?.length > 0 && (
+        <>
+          <Typography>{t('excludable_ingredients')}</Typography>
+          <IngredientsTree ingredients={excludableIngredients} />
+        </>
+      )}
+      {putasideIngredients?.length > 0 && (
+        <>
+          <Typography>{t('putaside_ingredients')}</Typography>
+          <IngredientsTree ingredients={putasideIngredients} />
+        </>
+      )}
+      {choiceIngredients?.length > 0 && (
+        <>
+          <Typography>{t('choice_ingredients')}</Typography>
+          <IngredientsTree ingredients={choiceIngredients} />
+        </>
+      )}
+      {sideDishIngredients?.length > 0 && (
+        <>
+          <Typography>{t('sideDish_ingredients')}</Typography>
+          <IngredientsTree ingredients={sideDishIngredients} />
+        </>
+      )}
+      {addableIngredientsIngredients?.length > 0 && (
+        <>
+          <Typography>{t('addable_ingredients')}</Typography>
+          <IngredientsTree ingredients={addableIngredientsIngredients} />
+        </>
+      )}
+      {addableDishesIngredients?.length > 0 && (
+        <>
+          <Typography>{t('addableDishes_ingredients')}</Typography>
+          <IngredientsTree ingredients={addableDishesIngredients} />
+        </>
+      )}
     </>
   )
 }
