42a86625a29b93fa72a44549f6535c73f7f17b12
Yotam Berkowitz <yotamberk@gmail.com>
2022-12-12 15:54:54 +0200

    add raw drink icons

--- client/src/components/Icons/DrinkSizeIcon/index.jsx
+++ client/src/components/Icons/DrinkSizeIcon/index.jsx
@@ -1,3 +1,4 @@
+// import { SvgIcon } from '@mui/material'
 import React from 'react'
 import BeerBottleIcon from './BeerBottleIcon'
 import BeerMugFoamIcon from './BeerMugFoamIcon'
@@ -306,4 +307,56 @@ export default function DrinkSizeIcon({ type }) {
   default:
     break
   }
+}
+
+export const DrinkSizeIconByName = ({ name, ...rest }) => {
+  return <Icon name={name} {...rest} />
+}
+
+function useDynamicSVGImport(name, options = {}) {
+  const ImportedIconRef = React.useRef()
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState()
+
+  const { onCompleted, onError } = options
+  React.useEffect(() => {
+    setLoading(true)
+    const importIcon = async () => {
+      try {
+        ImportedIconRef.current = (
+          await import(`../drink-icons/${name}.svg`)
+        ).ReactComponent
+        if (onCompleted) {
+          onCompleted(name, ImportedIconRef.current)
+        }
+      } catch (err) {
+        if (onError) {
+          onError(err)
+        }
+        setError(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+    importIcon()
+  }, [name, onCompleted, onError])
+
+  return { error, loading, SvgIcon: ImportedIconRef.current }
+}
+
+const Icon = ({ name, onCompleted, onError, ...rest }) => {
+  const { error, loading, SvgIcon } = useDynamicSVGImport(name, {
+    onCompleted,
+    onError,
+  })
+  if (error) {
+    return error.message
+  }
+  if (loading) {
+    return 'Loading...'
+  }
+  if (SvgIcon) {
+    return <SvgIcon {...rest} />
+  }
+  return null
 }
