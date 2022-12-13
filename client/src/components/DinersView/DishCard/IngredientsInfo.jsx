809f09ba0751aff3403fd7f646485f210a72565b
Yotam Berkowitz <yotamberk@gmail.com>
2022-12-13 14:32:19 +0200

    show ingredients of all nested recipe components

--- client/src/components/DinersView/DishCard/IngredientsInfo.jsx
+++ client/src/components/DinersView/DishCard/IngredientsInfo.jsx
@@ -1,34 +1,35 @@
 import React from 'react'
+import _flatten from 'lodash/flatten'
 import { useV1IngredientsByIds } from 'hooks/ingredients'
 import IngredientsTree from 'components/DinersView/DishCard/IngredientsTree'
 
 function IngredientsInfo({ data }) {
   const { ingredients: mandatoryIngredients } = useV1IngredientsByIds(
-    data.recipe.mandatory.map((c) => c.id)
+    _flatten(data.recipe.mandatory).map((c) => c.id)
   )
 
   const { ingredients: excludableIngredients } = useV1IngredientsByIds(
-    data.recipe.excludable.map((c) => c.id)
+    _flatten(data.recipe.excludable).map((c) => c.id)
   )
 
   const { ingredients: putasideIngredients } = useV1IngredientsByIds(
-    data.recipe.putaside.map((c) => c.id)
+    _flatten(data.recipe.putaside).map((c) => c.id)
   )
 
   const { ingredients: choiceIngredients } = useV1IngredientsByIds(
-    data.recipe.choice.map((c) => c.id)
+    _flatten(data.recipe.choice).map((c) => c.id)
   )
 
   const { ingredients: sideDishIngredients } = useV1IngredientsByIds(
-    data.recipe.sideDish.map((c) => c.id)
+    _flatten(data.recipe.sideDish).map((c) => c.id)
   )
 
   const { ingredients: addableIngredientsIngredients } = useV1IngredientsByIds(
-    data.recipe.addableIngredients.map((c) => c.id)
+    _flatten(data.recipe.addableIngredients).map((c) => c.id)
   )
 
   const { ingredients: addableDishesIngredients } = useV1IngredientsByIds(
-    data.recipe.addableDishes.map((c) => c.id)
+    _flatten(data.recipe.addableDishes).map((c) => c.id)
   )
 
   const allTopLevelIngredients = React.useMemo(
